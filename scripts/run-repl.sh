1bad4e4ed0ef251a34ec0d8817321fc30f0bfc42
Dan Kogai <dankogai+github@gmail.com>
2018-06-06 09:26:17 +0900

    fix run-repl.sh for linux

--- scripts/run-repl.sh
+++ scripts/run-repl.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-
-swift build && swift -I.build/debug -L.build/debug -lBigInt -lBigNum
+#!/bin/bash
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    inc=-I/usr/lib/swift/clang/include
+else
+    inc=""
+fi
+swift build && swift ${inc} -I.build/debug -L.build/debug -lBigInt -lBigNum
